bccf3caa8a4f81dc2d9c2279b881f20fd09b89e3
luwenchao <lwc_funnylife@126.com>
2018-06-29 11:26:22 +0800

    Initial Commit

---
+++ FastForwardingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastForwardingDemo
+//
+//  Created by cheyipai on 2018/6/29.
+//  Copyright © 2018年 cheyipai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
